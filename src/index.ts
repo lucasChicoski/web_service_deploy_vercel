3b32dc45af8ef9b127419ad8e24cc30103a8cd4e
Lucas Chicoski <lucas.chicoski9@gmail.com>
2024-10-31 11:24:52 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import express from "express";
 import ServerlessHttp from "serverless-http";
 const app = express();
 
-app.get("/teste", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log("Server running on port 3000");
 })
